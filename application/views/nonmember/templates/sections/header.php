3adc07e672192b1bc37e091cd33727729c8a7158
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-12-12 14:57:14 +0600

    client's feedback fixing

--- application/views/nonmember/templates/sections/header.php
+++ application/views/nonmember/templates/sections/header.php
@@ -1,18 +1,18 @@
 <div class="header-section" >
-<div class="container" >
-    <div class="row header-wrap" >
-        <div class="col-sm-6 col-md-6 form-group">
-            <a href="<?php echo base_url(); ?>">
-                <img class="img-responsive logo-wrap" src="<?php echo base_url() ?>resources/images/<?php echo $page_details[0]['logo'] ?>" alt="Cadogan Mcqueen Logo"> 
-            </a>         
-        </div>
-        <div class="col-sm-6 col-md-6">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="float_phone"><?php echo $page_details[0]['telephone'] ?></div>
+    <div class="container" >
+        <div class="row header-wrap" >
+            <div class="col-sm-6 col-md-6 form-group">
+                <a href="<?php echo base_url(); ?>">
+                    <img class="img-responsive logo-wrap" src="<?php echo base_url() ?>resources/images/<?php echo $page_details[0]['logo'] ?>" alt="Cadogan Mcqueen Logo"> 
+                </a>         
+            </div>
+            <div class="col-sm-6 col-md-6">
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <div class="float_phone"><?php echo $page_details[0]['telephone'] ?> <br> <span class="email-address">bdlions@gmail.com</span></div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
